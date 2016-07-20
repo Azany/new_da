43d0c08b304774fa2c9ee2a1a8a0ad1e9c43b02d
Azany <abysstorg5@mail.ru>
2016-07-20 09:29:30 +0300

    update

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/usr/lib/jvm/java-8-oracle/jre/bin/java -Xms1G -Xmx2G -cp Launcher.jar:LauncherAuthlib.jar:Thermos.jar cpw.mods.fml.relauncher.ServerLaunchWrapper
+/usr/lib/jvm/java-8-oracle/jre/bin/java -Xms1G -Xmx2G -jar Thermos.jar
